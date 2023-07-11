5b3e7e14f18cbdbdecf8bf617ba72db8a2109e86
nonchris <git@chris-ge.de>
2023-07-11 13:23:02 +0200

    add type-conversion tasks

---
+++ exercises/basics/type_conversion_1.py
@@ -0,0 +1,42 @@
+"""
+Remember:
+You can always run the code to try!
+Don't worry if not everything is working yet :)
+Always try to understand the error messages!
+The code behind the # is a hint on how to start,
+you can remove the # and space behind it and use it as a start!
+"""
+
+number = "41"
+
+# TODO: Convert the string to an integer
+# number =
+
+# let's print it and ensure it's type
+print(number)
+print(type(number))
+
+# TODO: add 1 to the number
+# number =
+
+# let's check the value
+print(number)
+
+# TODO: convert it back to a string
+
+
+print(number)
+print(type(number))
+
+# TODO: add a zero to the string
+
+
+print(number)
+print(type(number))
+
+# TODO now double the string by adding it to itself
+
+
+print(number)
+print(type(number))
+
---
+++ exercises/basics/type_conversion_1_solution.py
@@ -0,0 +1,32 @@
+number = "42"
+
+# convert the string to an integer
+number = int(number)
+
+# let's print it and ensure it's type
+print(number)
+print(type(number))
+
+# add 1 to the integer
+number = number + 1
+
+# let's check the value
+print(number)
+
+# convert number back to string
+number = str(number)
+
+print(number)
+print(type(number))
+
+# add a zero to the string
+number = number + "0"
+print(number)
+print(type(number))
+
+# double the string by adding it to itself
+number = number + number
+print(number)
+print(type(number))
+
+# note that the type of the variable 'number' is changed from str to int and back
