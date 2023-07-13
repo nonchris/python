76236e85775a78ef3ca1acdead68f6694c52b5a1
nonchris <git@chris-ge.de>
2023-07-13 14:27:09 +0200

    add simple function 3

---
+++ exercises/basics/simple_function_3.py
@@ -0,0 +1,25 @@
+my_dict = {}
+
+# TODO: write a function that takes two arguments and adds them to the dict
+#  the first argument is the key, the second the value
+
+# def add_to_dict(key, value):
+
+
+# TODO: call the function twice with different arguments
+#  print the dict after each function call and inspect the result
+
+
+# TODO: write a function that takes the name of a key and prints its value
+# def print_value(
+
+
+# we can check if a key exists in a dict with the 'in' keyword
+# example:
+d = {"x": 1}
+if "x" in d:
+    print("x is in d!")
+
+# TODO: write a function that takes the name of a key and prints if it is in the dict
+
+
---
+++ exercises/basics/simple_function_3_solution.py
@@ -0,0 +1,35 @@
+my_dict = {}
+
+# write a function that takes two arguments and adds them to the dict
+# the first argument is the key, the second the value
+
+def add_to_dict(key, value):
+    my_dict[key] = value
+
+
+# call the function twice with different arguments
+# print the dict after each function call and inspect the result
+add_to_dict("x", 1)
+print(my_dict)
+add_to_dict("y", 2)
+print(my_dict)
+
+
+# write a function that takes the name of a key and prints its value
+def print_value(key):
+    val = my_dict[key]
+    print(val)
+
+# we can check if a key exists in a dict with the 'in' keyword
+# example:
+d = {"x": 1}
+if "x" in d:
+    print("x is in d!")
+
+# write a function that takes the name of a key and prints if it is in the dict
+# also print that the key is not in the dict if it isn't
+def print_if_in_dict(key):
+    if key in my_dict:
+        print(f"{key} is in my_dict")
+    else:
+        print(f"{key} is not in my_dict")
