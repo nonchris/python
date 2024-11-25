a6ba1c97a08bb7a19e2542c8e429551c23e7caa9
nonchris <git@chris-ge.de>
2024-11-25 17:25:33 +0100

    more explanation

--- exercises/basics/simple_type_conversion_1/de_einfaches_typ_konvertieren.py
+++ exercises/basics/simple_type_conversion_1/de_einfaches_typ_konvertieren.py
@@ -7,35 +7,48 @@ Der Code hinter dem # ist ein Hinweis, wie du starten kannst,
 du kannst das # und das Leerzeichen dahinter entfernen und es als Start benutzen!
 """
 
+# TODO: Genereller Hinweis:
+#  Die 41 ist ein Beispielwert, der komplett austauschbar ist.
+#  Du sollst in keiner Aufgabe diesen konkreten Wert eingeben!
+#  Wir wollen von der Variable zahl reden.
 zahl = "41"
 
 # TODO: Konvertiere die Variable zahl in einen Integer
-# zahl =
+#zahl =
 
 # Lass es uns ausdrucken und den Typ sicherstellen
+print("Wert und Typ der Variable zahl nach Konvertierung zu int:")
 print(zahl)
 print(type(zahl))
+print()  # leerzeile für übersicht
 
 # TODO: addiere 1 zur zahl
-# zahl =
+#zahl =
 
 # Lassen wir uns den Wert prüfen
+print("zahl nach der addition mit 1")
 print(zahl)
+print()  # leerzeile für übersicht
 
 # TODO: konvertiere es zurück in einen String
 
-
+print("Wert und Typ der Variable zahl nach Konvertierung zurück zu str:")
+print()
 print(zahl)
 print(type(zahl))
+print()  # leerzeile für übersicht
 
 # TODO: füge dem String eine Null an den string hinzu
 
 
+print("Wert von zahl nach dem Anhängen von einer Null")
 print(zahl)
 print(type(zahl))
+print()  # leerzeile für übersicht
 
 # TODO: verdopple jetzt den String, indem du ihn zu sich selbst hinzufügst
 
 
+print("Wert von Zahl nach Verdopplung")
 print(zahl)
 print(type(zahl))
--- exercises/basics/simple_type_conversion_1/simple_type_conversion_1.py
+++ exercises/basics/simple_type_conversion_1/simple_type_conversion_1.py
@@ -7,36 +7,47 @@ The code behind the # is a hint on how to start,
 you can remove the # and space behind it and use it as a start!
 """
 
+# TODO: Meta:
+#  The 41 is an example value it's completely exchangeable.
+#  No task requires you entering that specific value
+#  Instead we're always talking about the variable number
 number = "41"
 
 # TODO: Convert the string to an integer
-# number =
+#number =
 
 # let's print it and ensure it's type
+print("Value and type of number after conversion to int:")
 print(number)
 print(type(number))
+print()  # empty line for prettier output
 
 # TODO: add 1 to the number
-# number =
+#number =
 
 # let's check the value
+print("Value and type of number after adding 1:")
 print(number)
+print()  # empty line for prettier output
 
 # TODO: convert it back to a string
 
-
+print("Value and type of number after conversion to str:")
 print(number)
 print(type(number))
 
 # TODO: add a zero to the string
 
 
+print("Value and type of number after adding a zero to the string")
 print(number)
 print(type(number))
+print()  # empty line for prettier output
 
 # TODO now double the string by adding it to itself
 
 
+print("Value of number after doubling the string")
 print(number)
 print(type(number))
 
