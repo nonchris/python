127551966d235d414806a1339710ed2798ac8526
nonchris <git@chris-ge.de>
2024-12-02 15:45:37 +0100

    new translation

---
+++ exercises/basics/simple_function_2/de_einfache_function_2.py
@@ -0,0 +1,28 @@
+"""
+Erinnerung:
+Du kannst den Code immer ausführen, um es auszuprobieren!
+Mach dir keine Sorgen, wenn nicht alles sofort funktioniert :)
+Versuche immer, die Fehlermeldungen zu verstehen!
+Der Code hinter dem # ist ein Hinweis, wie du starten kannst,
+du kannst das # und das Leerzeichen dahinter entfernen und es als Start benutzen!
+"""
+
+x_1 = 2
+y_1 = 3
+
+x_2 = 4
+y_2 = 5
+
+x_3 = 6
+y_3 = 7
+
+# TODO: schreibe eine function addiere_zahlen(x, y): die zwei nummern addiert und das ergbnis zurück gibt
+
+
+# TODO: kommentiere den Code hier drunter ein und prüfe, ob das erwartete Ergebnis raus kommt.
+#erg1 = addiere_zahlen(x_1, y_1)
+#print(res1)
+
+# TODO: rufe die funktion mit den anderen beiden Zahlenpaaren auf
+#  speichere das zuürckgegebene Ergebis in einer Variable und printe die Variable
+
